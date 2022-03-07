ae2c07be62ac08ad4f55db83852d5cfe51e6cadc
Rokk <jmcgilberry@gmail.com>
2022-03-06 18:02:31 -0600

    Updates

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 MainMenuBarArtFrame:SetAlpha(0);
 MicroButtonAndBagsBar:SetAlpha(0);
 StanceBarFrame:SetAlpha(0);
-C_NamePlate.SetNamePlateFriendlySize(60, 30)
+C_NamePlate.SetNamePlateFriendlySize(60, 60)
 
-ZoneAbilityFrame:ClearAllPoints()ZoneAbilityFrame:SetPoint("RIGHT",UIParent,"CENTER",270,-110)ZoneAbilityFrame.SetPoint=function()end
+--ZoneAbilityFrame:ClearAllPoints()ZoneAbilityFrame:SetPoint("RIGHT",UIParent,"CENTER",270,-110)ZoneAbilityFrame.SetPoint=function()end
